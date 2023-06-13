f49447f2a978bd7f04ce974ada6d973a9a7c7c9c
Dmytro <pohoriletsdmytro@gmail.com>
2023-06-13 11:03:12 +0300

    AdvancedSessionPlugin

---
+++ Source/RootOfWin/ROWGameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Steam/steam_api.h"
+#include "ROWGameMode.generated.h"
+#define RAW_APP_ID "2423050"
+/**
+ * 
+ */
+UCLASS()
+class ROOTOFWIN_API AROWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AROWGameMode();
+	
+};
